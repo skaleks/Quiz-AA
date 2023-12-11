c6517f10b62df9bf1300b7e0f6dd40e825e34e54
Kir Streltsov <skaleksandrovich@inbox.ru>
2023-12-11 12:57:00 +0300

    Add context and sketch of archi

---
+++ Assets/Scripts/Service/Scene/SceneHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Service
+{
+    public class SceneHandler
+    {
+        public void LoadScene(string title)
+        {
+            SceneManager.LoadSceneAsync(title, LoadSceneMode.Single);
+        }
+    }
+}
